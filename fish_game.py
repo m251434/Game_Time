1793f034cd9895a4409931bde0c40a1f5e45e53d
m251434 <m251434@usna.edu>
2022-12-07 18:30:43 -0500

    Main Fish Game

---
+++ fish_game.py
@@ -0,0 +1,52 @@
+import sys
+import pygame
+from pygame import mixer
+from fish import Fish
+from enemy_test import Enemy
+
+TILE = 64
+pygame.init()
+screen = pygame.display.set_mode((TILE*10, TILE*8))
+
+water = pygame.image.load('assets/water_tile.png')
+water_rect = water.get_rect()
+screen_rect = screen.get_rect()
+
+fish = Fish()
+enemy = Enemy()
+
+game_objects = pygame.sprite.Group()
+game_objects.add(enemy, fish)
+num_tiles = screen_rect.width // water_rect.width
+
+
+def draw_background():
+    for y in range(num_tiles):
+        for x in range(num_tiles):
+            screen.blit(water, (x*water_rect.width, y*water_rect.height))
+
+coordinate = (0, 0)
+clock = pygame.time.Clock()
+
+mixer.init()
+mixer.music.load('assets/Undertale OST_ 100 - Megalovania.mp3')
+mixer.music.play()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEMOTION:
+            coordinate = pygame.mouse.get_pos()
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    fish.move(coordinate)
+    enemy.move(screen_rect.width, screen_rect.height)
+    collision = pygame.sprite.collide_rect(fish, enemy)
+    if collision:
+        print("You Hit The Fish!")
+
+    draw_background()
+    fish.draw(screen)
+    enemy.draw(screen)
+    pygame.display.flip()
+    clock.tick(70)
